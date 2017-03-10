3a0d91912c8716a1c9b682f2e0bb9cc72dec6f9d
Geo Pertea <geo.pertea@gmail.com>
2017-03-09 22:32:44 -0500

    -Q now just works with -R to add "##Qry|" prefix with query region info

--- tabix.c
+++ tabix.c
@@ -1,7 +1,7 @@
 /*  tabix.c -- Generic indexer for TAB-delimited genome position files.
 
     Copyright (C) 2009-2011 Broad Institute.
-    Copyright (C) 2010-2012, 2014-2016 Genome Research Ltd.
+    Copyright (C) 2010-2012, 2014-2017 Genome Research Ltd.
 
     Author: Heng Li <lh3@sanger.ac.uk>
 
@@ -44,7 +44,7 @@ DEALINGS IN THE SOFTWARE.  */
 typedef struct
 {
     char *regions_fname, *targets_fname;
-    int print_header, header_only;
+    int print_header, header_only, qinfo;
 }
 args_t;
 
@@ -139,6 +139,7 @@ static char **parse_regions(char *regions_fname, char **argv, int argc, int *nre
     for (iseq=0; iseq<argc; iseq++) regs[ireg++] = strdup(argv[iseq]);
     return regs;
 }
+
 static int query_regions(args_t *args, char *fname, char **regs, int nregs)
 {
     int i;
@@ -203,10 +204,16 @@ static int query_regions(args_t *args, char *fname, char **regs, int nregs)
             for (i=0; i<nregs; i++)
             {
                 hts_itr_t *itr = tbx_itr_querys(tbx, regs[i]);
+                int qinfoshown = 0;
                 if ( !itr ) continue;
+
                 while (tbx_itr_next(fp, tbx, itr, &str) >= 0)
                 {
                     if ( reg_idx && !regidx_overlap(reg_idx,seq[itr->curr_tid],itr->curr_beg,itr->curr_end, NULL) ) continue;
+                    if (args->qinfo && qinfoshown==0) {
+                       printf("##Qry|%s\n",regs[i]);
+                       qinfoshown=1;
+                    }
                     puts(str.s);
                 }
                 tbx_itr_destroy(itr);
@@ -230,7 +237,7 @@ static int query_chroms(char *fname)
 {
     const char **seq;
     int i, nseq, ftype = file_type(fname);
-    if ( ftype & IS_TXT || !ftype )
+    if ( (ftype & IS_TXT) || !ftype )
     {
         tbx_t *tbx = tbx_index_load(fname);
         if ( !tbx ) error("Could not load .tbi index of %s\n", fname);
@@ -263,7 +270,7 @@ static int query_chroms(char *fname)
 
 int reheader_file(const char *fname, const char *header, int ftype, tbx_conf_t *conf)
 {
-    if ( ftype & IS_TXT || !ftype )
+    if ( (ftype & IS_TXT) || !ftype )
     {
         BGZF *fp = bgzf_open(fname,"r");
         if ( !fp || bgzf_read_block(fp) != 0 || !fp->block_length ) return -1;
@@ -359,6 +366,7 @@ static int usage(void)
     fprintf(stderr, "   -l, --list-chroms          list chromosome names\n");
     fprintf(stderr, "   -r, --reheader FILE        replace the header with the content of FILE\n");
     fprintf(stderr, "   -R, --regions FILE         restrict to regions listed in the file\n");
+    fprintf(stderr, "   -Q, --qinfo                for -R, print each query region before result\n");
     fprintf(stderr, "   -T, --targets FILE         similar to -R but streams rather than index-jumps\n");
     fprintf(stderr, "\n");
     return 1;
@@ -366,8 +374,8 @@ static int usage(void)
 
 int main(int argc, char *argv[])
 {
-    int c, min_shift = 0, is_force = 0, list_chroms = 0, do_csi = 0;
-    tbx_conf_t conf = tbx_conf_gff, *conf_ptr = NULL;
+    int c, detect = 1, min_shift = 0, is_force = 0, list_chroms = 0, do_csi = 0;
+    tbx_conf_t conf = tbx_conf_gff;
     char *reheader = NULL;
     args_t args;
     memset(&args,0,sizeof(args_t));
@@ -376,6 +384,7 @@ int main(int argc, char *argv[])
     {
         {"help", no_argument, NULL, 'h'},
         {"regions", required_argument, NULL, 'R'},
+        {"qinfo", no_argument, NULL, 'Q'},
         {"targets", required_argument, NULL, 'T'},
         {"csi", no_argument, NULL, 'C'},
         {"zero-based", no_argument, NULL, '0'},
@@ -395,53 +404,59 @@ int main(int argc, char *argv[])
     };
 
     char *tmp;
-    while ((c = getopt_long(argc, argv, "hH?0b:c:e:fm:p:s:S:lr:CR:T:", loptions,NULL)) >= 0)
+    while ((c = getopt_long(argc, argv, "hH?0b:c:e:fm:p:s:S:lr:CQR:T:", loptions,NULL)) >= 0)
     {
         switch (c)
         {
             case 'R': args.regions_fname = optarg; break;
+            case 'Q': args.qinfo = 1; break;
             case 'T': args.targets_fname = optarg; break;
             case 'C': do_csi = 1; break;
             case 'r': reheader = optarg; break;
             case 'h': args.print_header = 1; break;
             case 'H': args.print_header = 1; args.header_only = 1; break;
             case 'l': list_chroms = 1; break;
-            case '0': conf.preset |= TBX_UCSC; break;
+            case '0': conf.preset |= TBX_UCSC; detect = 0; break;
             case 'b':
                 conf.bc = strtol(optarg,&tmp,10);
                 if ( *tmp ) error("Could not parse argument: -b %s\n", optarg);
+                detect = 0;
                 break;
             case 'e':
                 conf.ec = strtol(optarg,&tmp,10);
                 if ( *tmp ) error("Could not parse argument: -e %s\n", optarg);
+                detect = 0;
                 break;
-            case 'c': conf.meta_char = *optarg; break;
+            case 'c': conf.meta_char = *optarg; detect = 0; break;
             case 'f': is_force = 1; break;
             case 'm':
                 min_shift = strtol(optarg,&tmp,10);
                 if ( *tmp ) error("Could not parse argument: -m %s\n", optarg);
                 break;
             case 'p':
-                      if (strcmp(optarg, "gff") == 0) conf_ptr = &tbx_conf_gff;
-                      else if (strcmp(optarg, "bed") == 0) conf_ptr = &tbx_conf_bed;
-                      else if (strcmp(optarg, "sam") == 0) conf_ptr = &tbx_conf_sam;
-                      else if (strcmp(optarg, "vcf") == 0) conf_ptr = &tbx_conf_vcf;
-                      else if (strcmp(optarg, "bcf") == 0) ;    // bcf is autodetected, preset is not needed
-                      else if (strcmp(optarg, "bam") == 0) ;    // same as bcf
-                      else error("The preset string not recognised: '%s'\n", optarg);
-                      break;
+                detect = 0;
+                if (strcmp(optarg, "gff") == 0) conf = tbx_conf_gff;
+                else if (strcmp(optarg, "bed") == 0) conf = tbx_conf_bed;
+                else if (strcmp(optarg, "sam") == 0) conf = tbx_conf_sam;
+                else if (strcmp(optarg, "vcf") == 0) conf = tbx_conf_vcf;
+                else if (strcmp(optarg, "bcf") == 0) detect = 1; // bcf is autodetected, preset is not needed
+                else if (strcmp(optarg, "bam") == 0) detect = 1; // same as bcf
+                else error("The preset string not recognised: '%s'\n", optarg);
+                break;
             case 's':
                 conf.sc = strtol(optarg,&tmp,10);
                 if ( *tmp ) error("Could not parse argument: -s %s\n", optarg);
+                detect = 0;
                 break;
             case 'S':
                 conf.line_skip = strtol(optarg,&tmp,10);
                 if ( *tmp ) error("Could not parse argument: -S %s\n", optarg);
+                detect = 0;
                 break;
             case 1:
                 printf(
 "tabix (htslib) %s\n"
-"Copyright (C) 2016 Genome Research Ltd.\n", hts_version());
+"Copyright (C) 2017 Genome Research Ltd.\n", hts_version());
                 return EXIT_SUCCESS;
             default: return usage();
         }
@@ -463,14 +478,14 @@ int main(int argc, char *argv[])
 
     char *fname = argv[optind];
     int ftype = file_type(fname);
-    if ( !conf_ptr )    // no preset given
+    if ( detect )  // no preset given
     {
-        if ( ftype==IS_GFF ) conf_ptr = &tbx_conf_gff;
-        else if ( ftype==IS_BED ) conf_ptr = &tbx_conf_bed;
-        else if ( ftype==IS_SAM ) conf_ptr = &tbx_conf_sam;
+        if ( ftype==IS_GFF ) conf = tbx_conf_gff;
+        else if ( ftype==IS_BED ) conf = tbx_conf_bed;
+        else if ( ftype==IS_SAM ) conf = tbx_conf_sam;
         else if ( ftype==IS_VCF )
         {
-            conf_ptr = &tbx_conf_vcf;
+            conf = tbx_conf_vcf;
             if ( !min_shift && do_csi ) min_shift = 14;
         }
         else if ( ftype==IS_BCF )
@@ -490,10 +505,7 @@ int main(int argc, char *argv[])
     if ( min_shift!=0 && !do_csi ) do_csi = 1;
 
     if ( reheader )
-        return reheader_file(fname, reheader, ftype, conf_ptr);
-
-    if ( conf_ptr )
-        conf = *conf_ptr;
+        return reheader_file(fname, reheader, ftype, &conf);
 
     char *suffix = ".tbi";
     if ( do_csi ) suffix = ".csi";
